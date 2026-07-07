fcd4e7456ea5c6b48f5bc531fbede890d7aad871
kzfly1 <83436850+kzfly1@users.noreply.github.com>
2026-07-07 22:27:33 +1000

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.3
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hello"
+print(type(variable))
+variable = ["hi", 1, 0]
+print(type(variable))
