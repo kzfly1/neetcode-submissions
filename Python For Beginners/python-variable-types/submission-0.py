5dfbb5423f42d6874279c362eb7a4729c80519ba
kzfly1 <83436850+kzfly1@users.noreply.github.com>
2026-07-07 22:26:15 +1000

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 0
+float_type = 33.3
+bool_type = False
+string_type = "hi"
+list_type = [1, 2, 3]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
