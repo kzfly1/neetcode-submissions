d000804952ca5fd1660cc1bac3907562796f8a32
kzfly1 <83436850+kzfly1@users.noreply.github.com>
2026-07-08 15:40:08 +1000

    Add: python-intro-to-dictionaries - submission-1

---
+++ Python For Beginners/python-intro-to-dictionaries/submission-1.py
@@ -0,0 +1,23 @@
+from typing import List, Dict
+
+def create_dict(name: str, age: int) -> Dict[str, int]:
+    return {name: age}
+
+
+def list_to_dict(words: List[str]) -> Dict[str, int]:
+    res = {}
+    for i, word in enumerate(words):
+        current_word = words[i]
+        res[current_word] = i
+    
+    return res
+
+
+
+# don't modify code below this line
+print(create_dict("Alice", 25))
+print(create_dict("Jane", 35))
+print(create_dict("Joe", 45))
+
+print(list_to_dict(["Alice", "Jane", "Joe"]))
+print(list_to_dict(["Apple", "Banana", "Watermelon", "Pineapple"]))
